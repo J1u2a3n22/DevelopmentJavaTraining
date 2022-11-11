38a6e0e79c5f72e3a01ced94c408de55c8973d9e
Juan.Canedo <Juan.Canedo@nice.com>
2022-11-11 18:44:39 -0400

    2.2 Exercise

---
+++ JavaExercises/src/HighschoolTrainee.java
@@ -0,0 +1,20 @@
+
+
+public class HighschoolTrainee {
+    public static void main(String[] args) {
+        int StudentAge=23;
+        double Prom=7.8;
+        char FirstInitial='J';
+        char LastInitial='C';
+        boolean Attendance=true;
+
+        System.out.println("The student Age is "+StudentAge);
+        System.out.println("The Prom is "+Prom);
+        System.out.println("The first Initial is "+ FirstInitial);
+        System.out.println("The attendance is "+Attendance);
+
+    }
+
+
+
+}
