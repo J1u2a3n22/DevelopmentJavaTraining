c9ff193c5c3e5d59ed330706455d5bec3b931ea1
Juan.Canedo <Juan.Canedo@nice.com>
2022-11-11 18:59:06 -0400

    Exercises 2.5

--- JavaExercises/src/HighschoolTrainee.java
+++ JavaExercises/src/HighschoolTrainee.java
@@ -4,20 +4,22 @@ public class HighschoolTrainee {
     public static void main(String[] args) {
         int StudentAge=23;
         double Prom=7.8;
-        char FirstInitial='J';
-        char LastInitial='C';
+        String StudentName="Juan Luis";
+        String LastName="Canedo Villarroel"  ;
+        char FirstInitial=StudentName.charAt(0);
+        char LastInitial=LastName.charAt(0);
         boolean Attendance=true;
 
-        String StudentName="Juan Luis";
         String FirstName="Juan Luis";
-        String LastName="Canedo Villarroel";
+
         System.out.println("The student Age is "+StudentAge);
         System.out.println("The Prom is "+Prom);
-        System.out.println("The first Initial is "+ FirstInitial);
+        //System.out.println("The first Initial is "+ FirstInitial);
         System.out.println("The attendance is "+Attendance);
         System.out.println("The student name is "+StudentName);
         System.out.println("The studen first name is "+FirstName);
         System.out.println("The studen last name is "+LastName);
+        System.out.println("the student : "+StudentName+" "+LastName+" And their Initials are: " + FirstInitial + LastInitial);
     }
 
 
